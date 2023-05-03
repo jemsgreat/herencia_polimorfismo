24cd19c4886884e9c9a1aead35d5a4e390ff2e91
Jhonattan Mercado <jmercadosenior@gmail.com>
2023-05-02 20:17:50 -0500

    video 2 aula 4 sobre polimorfismo

--- index.js
+++ index.js
@@ -7,18 +7,32 @@ import { CuentaNomina } from './Cuentas/CuentaNomina.js';
 import { Empleado } from './Empleados/Empleado.js';
 import { Gerente } from './Empleados/Gerente.js';
 import { Director } from './Empleados/Director.js';
+import { SistemaAutenticacion } from './Empleados/SistemaAutenticacion.js';
 
 
 const cliente = new Cliente('Leonardo','13804050','123224');
+cliente.asignarClave('1234');
+console.log(SistemaAutenticacion.login(cliente,'12345'));
+
 const cliente2 = new Cliente('María','16979808','8989');
 
 const empleado = new Empleado('Juan Perez','1234343',10000);
+empleado.asignarClave('1234');
+console.log(SistemaAutenticacion.login(empleado,'12345'));
+
+
 const gerente = new Gerente('Pedro Rivas','232344',12000);
+gerente.asignarClave('12345');
+console.log(SistemaAutenticacion.login(gerente,'12345'));
+
+
+
 const director = new Director('Elena Moreno','232323',15000);
 
 console.log(empleado.verBonificacion());
 console.log(gerente.verBonificacion());
 console.log(director.verBonificacion());
+
 
 
 
